8a9433c0cc911b8c3ad02787f482173aa1801ce8
Omar-Dakane-Muhumed <awsaalah451@gmail.com>
2024-10-26 11:08:48 -0400

    added the cuolumn dara of table books to the task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,10 @@
-SELECT * FROM books
+
+
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
